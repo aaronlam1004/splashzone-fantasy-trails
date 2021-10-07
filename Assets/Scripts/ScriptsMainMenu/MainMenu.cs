4d78aaea2bf37540652c1e8003ef45656fc63b4a
k0ji13 <thekoji007@gmail.com>
2021-10-07 14:23:39 -0700

    Main Menu (Initial Setup)
    Only the play button (venture forth) and quite button work so far.

---
+++ Assets/Scripts/ScriptsMainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void VentureForth()
+    {
+        SceneManager.LoadScene("Character Select");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Example");
+        Application.Quit();
+    }
+}
